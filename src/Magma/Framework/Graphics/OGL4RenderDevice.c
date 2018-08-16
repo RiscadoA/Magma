2d84cef4b62b40ee0b57e016fc7ef7a72e315846
Ricardo Antunes <32821840+RiscadoA@users.noreply.github.com>
2018-08-16 10:52:59 +0100

    More work on the OGL4 render device

--- src/Magma/Framework/Graphics/OGL4RenderDevice.c
+++ src/Magma/Framework/Graphics/OGL4RenderDevice.c
@@ -1,13 +1,59 @@
 #include "OGL4RenderDevice.h"
+	
+#include "../Memory/PoolAllocator.h"
 
-mfgError mfgCreateOGL4RenderDevice(mfgRenderDevice ** renderDevice, const mfgRenderDeviceDesc * desc, void * allocator)
+#include <stdlib.h>
+
+typedef struct
 {
-	// TO DO
-	return MFG_ERROR_INVALID_DATA;
+	mfgRenderDevice base;
+	void* allocator;
+	mfmPoolAllocator* pool;
+	mfmU8 poolMemory[280000];
+} mfgOGL4RenderDevice;
+
+mfgError mfgCreateOGL4RenderDevice(mfgRenderDevice ** renderDevice, mfiWindow* window, const mfgRenderDeviceDesc * desc, void * allocator)
+{
+	// Check if params are valid
+	if (renderDevice == NULL || window == NULL || desc == NULL || window->type != MFI_OGLWINDOW)
+		return MFG_ERROR_INVALID_ARGUMENTS;
+	
+	// Allocate render device
+	mfgOGL4RenderDevice* rd;
+	if (mfmAllocate(allocator, &rd, sizeof(mfgOGL4RenderDevice)) != MFM_ERROR_OKAY)
+		return MFG_ERROR_ALLOCATION_FAILED;
+
+	// Create pool allocator
+	{
+		mfmPoolAllocatorDesc desc;
+		desc.expandable = MFM_FALSE;
+		desc.slotCount = 8192;
+		desc.slotSize = 32;
+		mfmError err = mfmCreatePoolAllocatorOnMemory(&rd->pool, &desc, rd->poolMemory, sizeof(rd->poolMemory));
+		if (err != MFM_ERROR_OKAY)
+			return MFG_ERROR_ALLOCATION_FAILED;
+	}
+
+	// Initialize some properties
+	rd->base.object.destructorFunc = &mfgDestroyOGL4RenderDevice;
+	rd->base.object.referenceCount = 0;
+	rd->allocator = allocator;
+
+	*renderDevice = rd;
+	return MFG_ERROR_OKAY;
 }
 
-mfgError mfgDestroyOGL4RenderDevice(mfgRenderDevice * renderDevice)
+void mfgDestroyOGL4RenderDevice(void * renderDevice)
 {
+	mfgOGL4RenderDevice* rd = (mfgOGL4RenderDevice*)renderDevice;
+
+	// Destroy pool allocator
+	mfmDestroyPoolAllocator(rd->pool);
+
+	// Deallocate render device
+	if (mfmDeallocate(rd->allocator, rd) != MFM_ERROR_OKAY)
+		abort();
+
 	// TO DO
-	return MFG_ERROR_INVALID_DATA;
+	return MFG_ERROR_OKAY;
 }
--- src/Magma/Framework/Graphics/OGL4RenderDevice.h
+++ src/Magma/Framework/Graphics/OGL4RenderDevice.h
@@ -2,6 +2,8 @@
 
 #include "RenderDevice.h"
 
+#include "../Input/Window.h"
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -11,23 +13,20 @@ extern "C"
 	///		Creates a new OpenGL 4 render device.
 	/// </summary>
 	/// <param name="renderDevice">Pointer to render device handle</param>
+	/// <param name="window">Window handle</param>
 	/// <param name="desc">Render device description</param>
 	/// <param name="allocator">Allocator to use on allocations</param>
 	/// <returns>
 	///		MFG_ERROR_OKAY if there were no errors.
 	///		Otherwise returns an error code.
 	/// </returns>
-	mfgError mfgCreateOGL4RenderDevice(mfgRenderDevice** renderDevice, const mfgRenderDeviceDesc* desc, void* allocator);
+	mfgError mfgCreateOGL4RenderDevice(mfgRenderDevice** renderDevice, mfiWindow* window, const mfgRenderDeviceDesc* desc, void* allocator);
 
 	/// <summary>
 	///		Destroys an OpenGL 4 render device.
 	/// </summary>
 	/// <param name="renderDevice">Render device handle</param>
-	/// <returns>
-	///		MFG_ERROR_OKAY if there were no errors.
-	///		Otherwise returns an error code.
-	/// </returns>
-	mfgError mfgDestroyOGL4RenderDevice(mfgRenderDevice* renderDevice);
+	void mfgDestroyOGL4RenderDevice(void* renderDevice);
 
 #ifdef __cplusplus
 }
