5d2459a35256e18a0272a4dff99ce27d438f03ce
RiscadoA <32821840+RiscadoA@users.noreply.github.com>
2018-08-25 11:41:09 +0100

    Revert "Revert "Enhanced framework examples CMakeLists.txt""
    This reverts commit a2392044f3b12a41c77d7469783ae3ebd43a9aaf.

---
+++ src/Examples/Framework/Virtual-Machine/Example-C-1/Main.c
@@ -0,0 +1,12 @@
+﻿#include <Magma/Framework/Entry.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char** argv)
+{
+	if (mfInit(argc, argv) != MF_ERROR_OKAY)
+		abort();
+
+	mfTerminate();
+	return 0;
+}
