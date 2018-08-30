55688b77632c445829e1bb062f5cc2759493edde
Ricardo Antunes <32821840+RiscadoA@users.noreply.github.com>
2018-08-30 09:59:36 +0100

    Added Compiler.h

--- src/Examples/Framework/MSL/Example-C-1/Main.c
+++ src/Examples/Framework/MSL/Example-C-1/Main.c
@@ -1,8 +1,6 @@
 ﻿#include <Magma/Framework/Entry.h>
 #include <Magma/Framework/String/Stream.h>
-#include <Magma/Framework/Graphics/2.X/MSL/Lexer.h>
-#include <Magma/Framework/Graphics/2.X/MSL/Parser.h>
-#include <Magma/Framework/Graphics/2.X/MSL/Generator.h>
+#include <Magma/Framework/Graphics/2.X/MSL/Compiler.h>
 #include <Magma/Framework/Graphics/2.X/Bytecode.h>
 #include <Magma/Framework/Graphics/2.X/OGL4Assembler.h>
 #include <Magma/Framework/Graphics/2.X/D3D11Assembler.h>
@@ -29,51 +27,24 @@ int main(int argc, const char** argv)
 		u8"}"
 		;
 
-	mfgV2XToken tokens[2048];
-	mfgV2XLexerState lexerState;
-	if (mfgV2XRunMVLLexer(src, tokens, 2048, &lexerState) != MF_ERROR_OKAY)
-	{
-		mfsPutString(mfsErrStream, lexerState.errorMsg);
-		abort();
-	}
-
-	for (mfmU64 i = 0; i < lexerState.tokenCount; ++i)
-	{
-		if (tokens[i].attribute[0] != '\0')
-			mfsPrintFormatUTF8(mfsOutStream, u8"'%s' \"%s\"\n", tokens[i].info->name, tokens[i].attribute);
-		else
-			mfsPrintFormatUTF8(mfsOutStream, u8"'%s'\n", tokens[i].info->name);
-	}
-
-	mfgV2XNode nodes[2048];
-	mfgV2XParserState parserState;
-	mfgV2XCompilerState compilerState;
-	if (mfgV2XRunMVLParser(tokens, nodes, 2048, &lexerState, &parserState, &compilerState) != MF_ERROR_OKAY)
-	{
-		mfsPutString(mfsErrStream, parserState.errorMsg);
-		abort();
-	}
-
-	mfgV2XPrintNode(mfsOutStream, &nodes[0], 0);
-
 	mfmU8 bytecode[4096];
 	mfmU8 metaData[4096];
-	mfgV2XGeneratorState generatorState;
+	mfgV2XMVLCompilerInfo compilerInfo;
 	
-	if (mfgV2XRunMVLGenerator(&nodes[0], bytecode, sizeof(bytecode), metaData, sizeof(metaData), &generatorState, &compilerState, MFG_VERTEX_SHADER) != MF_ERROR_OKAY)
+	if (mfgV2XRunMVLCompiler(src, bytecode, sizeof(bytecode), metaData, sizeof(metaData), MFG_VERTEX_SHADER, &compilerInfo) != MF_ERROR_OKAY)
 	{
-		mfsPutString(mfsErrStream, generatorState.errorMsg);
+		mfsPutString(mfsErrStream, compilerInfo.errorMsg);
 		abort();
 	}
 
 	mfgMetaData* md;
-	if (mfgLoadMetaData(metaData, generatorState.metaDataSize, &md, NULL) != MF_ERROR_OKAY)
+	if (mfgLoadMetaData(metaData, compilerInfo.metaDataSize, &md, NULL) != MF_ERROR_OKAY)
 		abort();
 
-	if(mfgV2XOGL4Assemble(bytecode, generatorState.bytecodeSize, md, mfsOutStream) != MF_ERROR_OKAY)
+	if(mfgV2XOGL4Assemble(bytecode, compilerInfo.bytecodeSize, md, mfsOutStream) != MF_ERROR_OKAY)
 		abort();
 
-	if (mfgV2XD3D11Assemble(bytecode, generatorState.bytecodeSize, md, mfsOutStream) != MF_ERROR_OKAY)
+	if (mfgV2XD3D11Assemble(bytecode, compilerInfo.bytecodeSize, md, mfsOutStream) != MF_ERROR_OKAY)
 		abort();
 
 	mfgUnloadMetaData(md);
---
+++ src/Magma/Framework/Graphics/2.X/MSL/Compiler.c
@@ -0,0 +1,50 @@
+#include "Compiler.h"
+#include "Generator.h"
+
+#include <string.h>
+
+mfError mfgV2XRunMVLCompiler(const mfsUTF8CodeUnit * msl, mfmU8 * bytecode, mfmU64 maxBytecodeSize, mfmU8 * metaData, mfmU64 maxMetaDataSize, mfgV2XEnum shaderType, mfgV2XMVLCompilerInfo * info)
+{
+	if (msl == NULL || bytecode == NULL || maxBytecodeSize == 0 || metaData == NULL || maxMetaDataSize == 0 || info == NULL)
+		return MFG_ERROR_INVALID_ARGUMENTS;
+
+	mfError err;
+
+	mfgV2XToken tokens[4096];
+	mfgV2XNode nodes[4096];
+
+	mfgV2XLexerState lexerState;
+	mfgV2XParserState parserState;
+	mfgV2XGeneratorState generatorState;
+	mfgV2XCompilerState compilerState;
+
+	memset(lexerState.errorMsg, 0, MFG_V2X_MAX_ERROR_MESSAGE_SIZE);
+	memset(parserState.errorMsg, 0, MFG_V2X_MAX_ERROR_MESSAGE_SIZE);
+	memset(generatorState.errorMsg, 0, MFG_V2X_MAX_ERROR_MESSAGE_SIZE);
+
+	err = mfgV2XRunMVLLexer(msl, tokens, sizeof(tokens) / sizeof(mfgV2XToken), &lexerState);
+	if (err != MF_ERROR_OKAY)
+	{
+		memcpy(info->errorMsg, lexerState.errorMsg, MFG_V2X_MAX_ERROR_MESSAGE_SIZE);
+		return err;
+	}
+
+	err = mfgV2XRunMVLParser(tokens, nodes, sizeof(nodes) / sizeof(mfgV2XNode), &lexerState, &parserState, &compilerState);
+	if (err != MF_ERROR_OKAY)
+	{
+		memcpy(info->errorMsg, parserState.errorMsg, MFG_V2X_MAX_ERROR_MESSAGE_SIZE);
+		return err;
+	}
+
+	err = mfgV2XRunMVLGenerator(&nodes[0], bytecode, maxBytecodeSize, metaData, maxMetaDataSize, &generatorState, &compilerState, shaderType);
+	if (err != MF_ERROR_OKAY)
+	{
+		memcpy(info->errorMsg, generatorState.errorMsg, MFG_V2X_MAX_ERROR_MESSAGE_SIZE);
+		return err;
+	}
+	
+	info->bytecodeSize = generatorState.bytecodeSize;
+	info->metaDataSize = generatorState.metaDataSize;
+
+	return MF_ERROR_OKAY;
+}
